51d42d94b7e488821028be21553b54dbac34c0a6
VictorFrank <Victorbruun@github.com>
2024-10-16 16:30:39 +0200

    Updates home page with styling module

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home: React.FC = () => {
+
+
+    return (
+        <div className={styles.headline}>
+            <h1>Hello! Thanks for checking in!</h1>
+        </div>
+    );
+}
+
+export default Home;
