6beb65459084bd9b0a2df7c905a139d907def330
VictorFrank <Victorbruun@github.com>
2024-10-16 17:23:09 +0200

    Adds Spline component to allow for 3D objet to be rendered and used on site

---
+++ src/components/spline/SplineHomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+const SplineScene: React.FC = () => {
+    return (
+        <main>
+            <Spline scene="https://prod.spline.design/PvxXjxOOmbJXIWWR/scene.splinecode"/>
+        </main>
+    );
+};
+
+export default SplineScene;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Home.module.css';
+import SplineScene from '../../components/spline/SplineHomePage';
 
 const Home: React.FC = () => {
 
 
     return (
-        <div className={styles.headline}>
-            <h1>Hello! Thanks for checking in!</h1>
+        <div className={styles.splineContainer}>
+            <SplineScene></SplineScene>
         </div>
     );
 }
