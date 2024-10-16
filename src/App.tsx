cdd1033bbdc737aeaa32211634c68118bfbe8f60
VictorFrank <Victorbruun@github.com>
2024-10-16 16:08:24 +0200

    Adds home page, to initialize working on new web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div>
+                {/*Header navigator here*/}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                {/*Footer here*/}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
