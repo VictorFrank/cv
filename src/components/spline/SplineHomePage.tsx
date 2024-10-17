b97c1979e6a71987ab3f559036f40331eb48689e
VictorFrank <Victorbruun@github.com>
2024-10-17 14:27:30 +0200

    Adds navigation buttons to the home page

--- src/components/spline/SplineHomePage.tsx
+++ src/components/spline/SplineHomePage.tsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline';
 const SplineScene: React.FC = () => {
     return (
         <main>
-            <Spline scene="https://prod.spline.design/PvxXjxOOmbJXIWWR/scene.splinecode"/>
+            <Spline scene="https://prod.spline.design/DM4XlREsqegi1Fb8/scene.splinecode"/>
         </main>
     );
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import styles from './Home.module.css';
 import SplineScene from '../../components/spline/SplineHomePage';
 
 const Home: React.FC = () => {
+    const navigate = useNavigate()
+    const [showButtons, setShowButtons] = useState(false)
 
+    const handleButtonClick = () => {
+        setShowButtons(!showButtons)
+    }
 
     return (
-        <div className={styles.splineContainer}>
-            <SplineScene></SplineScene>
+        <div className={styles.background}>
+            <div className={styles.splineContainer}>
+                <button className={styles.invisibleButton} onClick={handleButtonClick}></button>
+                {showButtons && (
+                    <div className={styles.navigationButtons}>
+                        <button onClick={() => console.log('Button 1 clicked')}>Button 1</button>
+                        <button onClick={() => console.log('Button 2 clicked')}>Button 2</button>
+                        <button onClick={() => console.log('Button 3 clicked')}>Button 3</button>
+                    </div>
+                )}
+                <SplineScene></SplineScene>
+            </div>
         </div>
     );
 }
