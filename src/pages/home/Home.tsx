a345c0110f36683c28815fcf265859c0d1080f62
VictorFrank <Victorbruun@github.com>
2024-10-30 17:58:25 +0100

    Starting from scratch

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
-import styles from './Home.module.css';
-import SplineScene from '../../components/spline/SplineHomePage';
+import React from 'react';
 
 const Home: React.FC = () => {
-    const [showButtons, setShowButtons] = useState(false)
-
-    const handleButtonClick = () => {
-        setShowButtons(!showButtons)
-    }
 
     return (
-        <div className={styles.background}>
-            <div className={styles.splineContainer}>
-                <button className={styles.invisibleButton} onClick={handleButtonClick}></button>
-                {showButtons && (
-                    <div className={styles.navigationButtons}>
-                        <button className={styles.navigationButton1} onClick={() => console.log('Button 1 clicked')}>Button 1</button>
-                        <button className={styles.navigationButton2} onClick={() => console.log('Button 2 clicked')}>Button 2</button>
-                        <button className={styles.navigationButton3} onClick={() => console.log('Button 3 clicked')}>Button 3</button>
-                    </div>
-                )}
-                <SplineScene></SplineScene>
-            </div>
-        </div>
+        <div>Heyo</div>
     );
 }
 
