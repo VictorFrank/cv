0a46586f2646454c7f713de0ed06e421ef2293b8
VictorFrank <Victorbruun@github.com>
2024-10-31 17:32:09 +0100

    We go again, new homepage style

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styles from './Home.module.css';
+import Globe from '../../components/shapes/Globe';
 
 const Home: React.FC = () => {
 
     return (
-        <div>Heyo</div>
+        <div className={styles.background}>
+            <div className={styles.box}>
+                <div className={styles.topBox}>
+                    <div className={styles.name}>Victor Frank</div>
+                    <div className={styles.nameSub}>Master's degree ––––––– Software Engineering</div>
+                </div>
+                <div className={styles.bottomBox}>
+                    <div className={styles.figureBox}>
+                        <div className={styles.figure}>
+                            <Globe></Globe>
+                        </div>
+                        <div className={styles.figureText}> Circle here? </div>
+                    </div>
+                    <div className={styles.secondaryBox}>
+                        <div className={styles.secondaryText}> More text here</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
