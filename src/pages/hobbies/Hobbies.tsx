2dd5fc3c71d4685d1b1726b60c86d591a2b51175
VictorFrank <Victorbruun@github.com>
2024-11-18 15:54:45 +0100

    Created layout for the hobbies page

--- src/pages/hobbies/Hobbies.tsx
+++ src/pages/hobbies/Hobbies.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styles from './Hobbies.module.css';
+import { useNavigate } from 'react-router-dom';
 
 
-const Hobbies = () => {
-
+const Hobbies: React.FC = () => {
+    const navigate = useNavigate()
 
     return (
-        <div>
-            <h1>Hobbies</h1>
+        <div className={styles.background}>
+            <div className={styles.frame}>
+                <div className={styles.box}>
+                    Music box
+                </div>
+                <div className={styles.box}>
+                    Video games box
+                </div>
+                <div className={styles.box}>
+                    Something different box
+                </div>
+            </div>
         </div>
     );
 }
