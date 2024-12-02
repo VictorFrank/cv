c75cde2f6fde75bea82d447c53f1090c01ad3b40
VictorFrank <Victorbruun@github.com>
2024-12-02 13:32:24 +0100

    Adds playlists to hte hobbies page

--- src/pages/hobbies/Hobbies.tsx
+++ src/pages/hobbies/Hobbies.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Hobbies.module.css';
 import { useNavigate } from 'react-router-dom';
+import Spotify from "../../components/music/Spotify";
 
 
 const Hobbies: React.FC = () => {
@@ -10,7 +11,7 @@ const Hobbies: React.FC = () => {
         <div className={styles.background}>
             <div className={styles.frame}>
                 <div className={styles.box}>
-                    Music box
+                    <Spotify />
                 </div>
                 <div className={styles.box}>
                     Video games box
