0ef9f99afaad0d70440e34753b112a9f70084183
VictorFrank <vfrankbr@gmail.com>
2025-08-11 17:44:31 +0200

    switching from go and react to dotnet and angular

--- backend/db.go
+++ backend/db.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
 func initDB() {
-	// Initialize the database connection here
-	// This is a placeholder function; actual implementation will depend on the database being used
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbHost := os.Getenv("POSTGRES_HOST")
+
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	fmt.Println("Successfully connected to the database")
+
+	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from Go backend!")
+	})
+
+	fmt.Println("Backend listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
