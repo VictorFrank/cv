44777078f2663579906051943c57641c0c511e89
VictorFrank <vfrankbr@gmail.com>
2025-08-30 22:06:52 +0200

    Adds mock data for analysis page

--- frontend-angular/src/app/mtg-analysis/mtg-analysis.component.ts
+++ frontend-angular/src/app/mtg-analysis/mtg-analysis.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-mtg-analysis',
   standalone: true,
-  imports: [],
+  imports: [ NgForOf ],
   templateUrl: './mtg-analysis.component.html',
   styleUrl: './mtg-analysis.component.css'
 })
@@ -13,6 +14,22 @@ export class MtgAnalysisComponent implements OnInit{
     constructor(private http: HttpClient) {}
 
     stringData: string = ""
+
+    mockPlayerStats = [
+        { name: 'Player 1', gamesPlayed: 10, wins: 6, losses: 4, winRate: '60%' },
+        { name: 'Player 2', gamesPlayed: 10, wins: 4, losses: 6, winRate: '40%' },
+        { name: 'Player 3', gamesPlayed: 10, wins: 5, losses: 5, winRate: '50%' },
+        { name: 'Player 4', gamesPlayed: 10, wins: 7, losses: 3, winRate: '70%' },
+        { name: 'Player 5', gamesPlayed: 10, wins: 3, losses: 7, winRate: '30%' }
+    ]
+
+    mockCommanderStats = [
+        { name: 'Krenko', gamesPlayed: 10, wins: 6, losses: 4, winRate: '60%' },
+        { name: 'Muldrotha', gamesPlayed: 10, wins: 4, losses: 6, winRate: '40%' },
+        { name: 'Loot', gamesPlayed: 10, wins: 5, losses: 5, winRate: '50%' },
+        { name: 'The Gitrog Monster', gamesPlayed: 10, wins: 7, losses: 3, winRate: '70%' },
+        { name: 'Burakos', gamesPlayed: 10, wins: 3, losses: 7, winRate: '30%' }
+    ]
 
     ngOnInit(): void {
         console.log("ngOnInit called")
