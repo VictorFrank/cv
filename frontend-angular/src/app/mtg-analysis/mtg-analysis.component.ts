b7237dec72e36b0014354d4b8414e5d8131e56e0
VictorFrank <vfrankbr@gmail.com>
2025-08-31 00:10:39 +0200

    Allows for fetching data and showing wins on analysis screen

--- frontend-angular/src/app/mtg-analysis/mtg-analysis.component.ts
+++ frontend-angular/src/app/mtg-analysis/mtg-analysis.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-mtg-analysis',
   standalone: true,
-  imports: [ NgForOf ],
+    imports: [NgForOf, JsonPipe],
   templateUrl: './mtg-analysis.component.html',
   styleUrl: './mtg-analysis.component.css'
 })
@@ -15,6 +15,7 @@ export class MtgAnalysisComponent implements OnInit{
 
     stringData: string = ""
     mtgData: any = []
+    playerWinCount: { [key: string]: number } = {}
 
     mockPlayerStats = [
         { name: 'Player 1', gamesPlayed: 10, wins: 6, losses: 4, winRate: '60%' },
@@ -45,7 +46,36 @@ export class MtgAnalysisComponent implements OnInit{
         })
 
         //Add real data retrieval here
+        this.http.get('http://localhost:5234/api/mtgdata/data').subscribe({
+            next: (response) => {
+                this.mtgData = response
+                this.calculatePlayerWins()
+                console.log(this.mtgData)
+            },
+            error: (err) => {
+                console.error('Error retrieving data from backend:', err);
+            }
+        })
+    }
 
+    calculatePlayerWins(): void {
+        this.playerWinCount = {};
+
+        for (const match of this.mtgData) {
+            const winner = match.win;
+
+            if (winner) {
+                if (!this.playerWinCount[winner]) {
+                    this.playerWinCount[winner] = 1;
+                } else {
+                    this.playerWinCount[winner]++;
+                }
+            }
+        }
+    }
+
+    objectKeys(obj: any): string[] {
+        return Object.keys(obj);
     }
 
 }
